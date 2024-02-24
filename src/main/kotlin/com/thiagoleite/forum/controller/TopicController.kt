bf249f1a027287e5a7e916ad0c58726412fa7910
Thiago Leite <thiago.0686@gmail.com>
2024-02-24 10:47:32 -0300

    feat complete crud

--- src/main/kotlin/com/thiagoleite/forum/controller/TopicController.kt
+++ src/main/kotlin/com/thiagoleite/forum/controller/TopicController.kt
@@ -2,14 +2,21 @@ package com.thiagoleite.forum.controller
 
 import com.thiagoleite.forum.dtos.NewTopicForm
 import com.thiagoleite.forum.dtos.TopicView
+import com.thiagoleite.forum.dtos.UpdateTopicForm
 import com.thiagoleite.forum.service.TopicService
 import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.util.UriComponentsBuilder
 import java.util.*
 
 @RestController
@@ -28,7 +35,25 @@ class TopicController(
     }
 
     @PostMapping
-    fun create(@RequestBody @Valid dto: NewTopicForm) {
-        service.createTopic(dto)
+    fun create(
+            @RequestBody @Valid form: NewTopicForm,
+            uriBuilder: UriComponentsBuilder
+    ): ResponseEntity<TopicView> {
+        val topic = service.createTopic(form)
+        val uri = uriBuilder.path("topics/${topic.id}").build().toUri()
+
+        return ResponseEntity.created(uri).body(topic)
+    }
+
+    @PutMapping
+    fun update(@RequestBody @Valid form: UpdateTopicForm): ResponseEntity<TopicView> {
+        var topicView = service.update(form)
+        return ResponseEntity.ok(topicView)
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun delete(@PathVariable id: Long) {
+        service.delete(id)
     }
 }
--- src/main/kotlin/com/thiagoleite/forum/service/TopicService.kt
+++ src/main/kotlin/com/thiagoleite/forum/service/TopicService.kt
@@ -2,9 +2,11 @@ package com.thiagoleite.forum.service
 
 import com.thiagoleite.forum.dtos.NewTopicForm
 import com.thiagoleite.forum.dtos.TopicView
+import com.thiagoleite.forum.dtos.UpdateTopicForm
 import com.thiagoleite.forum.mapper.TopicFormMapper
 import com.thiagoleite.forum.mapper.TopicViewMapper
 import com.thiagoleite.forum.model.Topic
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 import java.util.stream.Collectors
 import kotlin.collections.ArrayList
@@ -29,9 +31,35 @@ class TopicService(
         return topicViewMapper.map(topic)
     }
 
-    fun createTopic(form: NewTopicForm) {
+    fun createTopic(form: NewTopicForm): TopicView {
        val topic = topicFormMapper.map(form)
         topic.id = topics.size.toLong() + 1
         topics = topics.plus(topic)
+        return topicViewMapper.map(topic)
+    }
+
+    fun update(form: UpdateTopicForm): TopicView {
+        val topic = topics.stream().filter { it ->
+            it.id == form.id
+        }.findFirst().get()
+        val updated = Topic(
+            id = form.id,
+            title = form.title,
+            message = form.message,
+            author = topic.author,
+            course = topic.course,
+            responses = topic.responses,
+            status = topic.status,
+            createdAt = topic.createdAt
+        )
+        topics = topics.minus(topic).plus(updated)
+        return topicViewMapper.map(updated)
+    }
+
+    fun delete(id: Long) {
+        val topic = topics.stream().filter{ it ->
+            it.id == id
+        }.findFirst().get()
+        topics.minus(topic)
     }
 }
